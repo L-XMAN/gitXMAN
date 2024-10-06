d07580898e596031c7a49eabad49b677f270fcaa
XMAN <131112551+L-XMAN@users.noreply.github.com>
2024-10-06 14:23:39 +0800

    Update AboutMe.vue

--- BiShe/graduate/src/components/AboutMe.vue
+++ BiShe/graduate/src/components/AboutMe.vue
@@ -10,12 +10,6 @@
                     <p>&emsp;&emsp;各位指导老师们，大家好！我是学习时长两年半的软件技术大专生，廖红生。
                         Vue (发音为 /vjuː/，类似 view) 是一款用于构建用户界面的 JavaScript 框架。它基于标准 HTML、CSS 和 JavaScript
                         构建，并提供了一套声明式的、组件化的编程模型，帮助你高效地开发用户界面。无论是简单还是复杂的界面，Vue 都可以胜任。
-                        <!-- 喜欢java、HTML、JS、MySQL!
-                        开机!<br>System.out.println("hello
-                        world!");
-                        &emsp;&lt;p&gt;hello world!&lt;/p&gt;
-                        &emsp;console.log("hello
-                        world")&emsp;hello-hello-hello&emsp;world!!!啊哈哈哈哈哈！代码写好咯！这写代码，多是一件美事！运行！error！输出啊？输出啊？怎么不输出？。。。TNND！！！为什么不输出！！！*@%#……&@%￥@&*@￥@%%#@#&@%@(抓狂！) -->
                     </p>
                 </div>
             </div>
